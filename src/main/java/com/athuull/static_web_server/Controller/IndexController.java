38dec5a4b2a0f7b10613c335e03dacb3810318ce
athuull <athulvanil03@gmail.com>
2024-12-05 17:50:28 +0530

    bug fix

--- src/main/java/com/athuull/static_web_server/Controller/IndexController.java
+++ src/main/java/com/athuull/static_web_server/Controller/IndexController.java
@@ -1,21 +1,13 @@
 package com.athuull.static_web_server.Controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
-    @Value("${APP_NAME:default-app}")
-    private String appName;
-
-    @GetMapping("/")
-    public String serveIndex() {
-        System.out.println("Request is served by" + appName);
-        return "index.html";
+    @GetMapping("/{path:[^\\.]*}") // Matches everything except paths with a dot (e.g., .js, .css)
+    public String forward() {
+        return "forward:/index.html";
     }
-
-
 }
--- src/main/java/com/athuull/static_web_server/ViewControllerMapping.java
+++ src/main/java/com/athuull/static_web_server/ViewControllerMapping.java
@@ -1,16 +1,16 @@
-package com.athuull.static_web_server;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class ViewControllerMapping implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        // Forward /about to /about/index.html
-        registry.addViewController("/about").setViewName("forward:/about/index.html");
-        registry.addViewController("/posts").setViewName("forward:/posts/index.html");
-    }
-}
+//package com.athuull.static_web_server;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class ViewControllerMapping implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        // Forward /about to /about/index.html
+//        registry.addViewController("/about").setViewName("forward:/about/index.html");
+//        registry.addViewController("/posts").setViewName("forward:/posts/index.html");
+//    }
+//}
