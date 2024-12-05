5f4574bed8cddc4d667f4e9a2253ad19facc5f73
athuull <athulvanil03@gmail.com>
2024-12-05 17:31:18 +0530

    bug fix

--- src/main/java/com/athuull/static_web_server/ViewControllerMapping.java
+++ src/main/java/com/athuull/static_web_server/ViewControllerMapping.java
@@ -3,14 +3,15 @@ package com.athuull.static_web_server;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class ViewControllerMapping implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-
-        registry.addViewController("/about").setViewName("redirect:/about");
-        registry.addViewController("/about").setViewName("redirect:/about/index.html");
+        // Forward /about to /about/index.html
+        registry.addViewController("/about").setViewName("forwards:/about/index.html");
+        registry.addViewController("/posts").setViewName("forwards:/posts/index.html");
 
     }
 }
